8f952af07b555907c21e8570d547642fbcb8eaa9
LeandroSoria <leandro.soria@.com.ar>
2022-05-26 20:37:11 -0300

    subo el archivo con las preguntas respondidas

---
+++ respuesta_a_preguntas.js
@@ -0,0 +1,4 @@
+1) Git fue inventado por Linus Torvalds que es el creador del sistema operativo linux, 
+se creo para posibilitar la ramificacion y el etiquetado como procesos de primera importancia y tambien para almacenar historiales de cambios en proyectos de desarallodares.
+2) Github pertenece a microsoft, fu  comprada en 2008.
+3) Atravez de gitbash es una de las formas de utilizar github si entrara a su sistema.
